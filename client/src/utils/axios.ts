8283934aeb3c0e705a6d87e847a433f8994b77ff
Varun Inapakurthi <varun.inapakurthi@surveysparrow.com>
2024-10-23 23:04:47 +0530

    Refactor: Remove console.log statement in axios.ts

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-console.log('import.meta.env.VITE_SERVER_URL', import.meta.env.VITE_SERVER_URL);
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   timeout: 10000,
